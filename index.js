7ead9a200839ae7ebcd8972519ef1671a385d87c
codersinghal <harshitsinghal0006@gmail.com>
2021-11-20 14:49:31 +0530

    deploying

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ cors=require('cors')
 const express = require("express")
 const path = require('path');
 const app = express();
-app.use(express.static(path.resolve(__dirname, "client/build")));
+app.use(express.static(path.resolve(__dirname, "frontend/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "frontend/build", "index.html"));
 });
 
 app.use(express.json());
